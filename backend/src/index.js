1fa045b8b6013f187ae274ec77a0b4ecebb0d8af
André Souza <andre.cassulino@gmail.com>
2026-03-28 21:17:50 -0300

    deploy backend teste

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Backend rodando com CI/CD");
+  res.send("Backend rodando com CI/CD!!!!");
 });
 
 app.listen(3000, () => {
